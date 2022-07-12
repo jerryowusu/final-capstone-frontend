82bbf857106841faa0887005bb8d6d5bb0929f04
Jerry Owusu <jerryowusu6133@gmail.com>
2022-07-12 18:23:47 -0400

    create reducers for cars

--- src/redux/Cars/cars.js
+++ src/redux/Cars/cars.js
@@ -1,11 +1,36 @@
-import React from 'react';
+/* eslint-disable default-param-last */
+import { carsURL } from '../../logics/urls';
 
-function Home() {
-  return (
-    <div className="cars">
-      <h1>Cars</h1>
-    </div>
-  );
-}
+const FETCH_CARS = 'cars/FETCH_CARS';
+const POST_CAR = 'cars/POST_CAR';
 
-export default Home;
+const initialState = [];
+
+const getCars = (payload) => ({
+  type: FETCH_CARS,
+  payload,
+});
+
+export const postCar = (payload) => ({
+  type: POST_CAR,
+  payload,
+});
+
+export const fetchCars = () => (dispatch) => {
+  fetch(carsURL).then((res) => res.json()).then((data) => {
+    dispatch(getCars(data.data));
+  });
+};
+
+const carReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_CARS:
+      return [...state, ...action.payload];
+    case POST_CAR:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
+};
+
+export default carReducer;
