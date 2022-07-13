81333a789c3bef39021ebff920393c999e159cd1
Jerry Owusu <jerryowusu6133@gmail.com>
2022-07-13 12:26:49 -0400

    Update homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,16 +1,17 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
-import { fetchCars, postCar } from '../redux/Cars/cars';
+import './Homepage.css';
+import { fetchCars } from '../redux/Cars/cars';
+import { setCars } from '../redux/Reservations/reservation';
 import CarDetails from './CarDetails';
 
 const Homepage = () => {
-  const cars = useSelector((state) => state.carsReducer);
+  const cars = useSelector((state) => state.allReservation.cars);
   const dispatch = useDispatch();
 
   const handleFetchCars = () => {
     fetchCars().then((response) => {
-      dispatch(postCar(response.data));
+      dispatch(setCars(response.data));
     });
   };
 
@@ -18,11 +19,14 @@ const Homepage = () => {
     handleFetchCars();
   }, []);
 
+  console.log(cars);
+
   return (
     <div className="home-container">
       <h1>Available Cars</h1>
       <h3>For the best, luxurious and comfortable rides, look no further</h3>
-      <div className="car-container">
+      <div className="cards-container">
+        welcome
         <CarDetails cars={cars} />
       </div>
     </div>
