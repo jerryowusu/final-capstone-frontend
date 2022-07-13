64111beb9781d2fce535de12a641ff3401e48077
Jerry Owusu <jerryowusu6133@gmail.com>
2022-07-13 10:05:31 -0400

    Change homepage link to cars

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Cars from './redux/Cars/cars';
+// import Cars from './redux/Cars/cars';
 import Reservations from './redux/Reservations/reservation';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route exact path="/" exactly element={<Cars />} />
+        <Route exact path="/" exactly element={<Homepage />} />
         <Route path="/Reservations" component={Reservations} />
         <Route path="/login" element={<Login />} />
         <Route path="/registeration" element={<Signup />} />
