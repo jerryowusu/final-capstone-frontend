3dc385286b01553f2b26a861cd101674df92fa5e
cynthiainga <candyinga67@gmail.com>
2022-07-13 09:19:01 +0200

    Add styling for registration page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Cars from './redux/Cars/cars';
 import Reservations from './redux/Reservations/reservation';
@@ -10,15 +10,13 @@ import Signup from './components/Signup';
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" exactly element={<Cars />} />
-          <Route path="/Reservations" component={Reservations} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/registeration" element={<Signup />} />
-        </Routes>
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" exactly element={<Cars />} />
+        <Route path="/Reservations" component={Reservations} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registeration" element={<Signup />} />
+      </Routes>
     </>
   );
 }
