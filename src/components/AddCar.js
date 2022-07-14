3d2424e22550bfbe795b98cecb3ec2400d21b1d3
Jerry Owusu <jerryowusu6133@gmail.com>
2022-07-14 11:25:46 -0400

    Add car alert response

---
+++ src/components/AddCar.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import CarForm from './CarForm';
+import Response from './Response';
+
+const AddCar = () => {
+  const cars = useSelector((state) => state.userReducer);
+
+  const [show, setShow] = useState(true);
+
+  return (
+    <>
+      <h1>Create and Add New Cars </h1>
+      {
+            cars.status === 201 ? (
+              <Response show={show} setShow={setShow} />
+            ) : (
+              <CarForm />
+            )
+          }
+    </>
+  );
+};
+
+export default AddCar;
