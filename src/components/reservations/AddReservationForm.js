d56c943a65049753a9d1b1686f77b9b343178e5f
Tobinchilongo <tobinchilongojnr@gmail.com>
2022-07-15 23:29:22 +0200

    Added Styles to the `AddreservationForm`

--- src/components/reservations/AddReservationForm.js
+++ src/components/reservations/AddReservationForm.js
@@ -1,23 +1,26 @@
 import React, { useState } from 'react';
+import './AddReservationForm.css';
 
 const AddReservationForm = () => {
   const [city, setCity] = useState('');
   console.log(setCity);
   return (
-    <div>
+    <div className="form">
       <h2>Add Reservation</h2>
-      <form>
-        <select>
-          <option>Car 1</option>
-          <option>Car 2</option>
-        </select>
-        <input
-          type="text"
-          placeholder="Enter City"
-          value={city}
-        />
-        <input type="date" />
-        <button type="submit">Reserve</button>
+      <form className="input">
+        <div className="border-form">
+          <select>
+            <option>Car 1</option>
+            <option>Car 2</option>
+          </select>
+          <input
+            type="text"
+            placeholder="Enter City"
+            value={city}
+          />
+          <input type="date" />
+          <button type="submit">Reserve</button>
+        </div>
       </form>
     </div>
   );
