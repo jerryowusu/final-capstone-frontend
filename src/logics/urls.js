0fa7a1c3adb17fde919918c5fd436cac71258871
jerryowusu <jerryowusu6133@gmail.com>
2022-07-16 01:21:19 +0000

    Update reservations url

--- src/logics/urls.js
+++ src/logics/urls.js
@@ -1,5 +1,5 @@
 const userURL = 'http://localhost:3001/api/v1/users';
 export const carsURL = 'http://localhost:3001/api/v1/cars';
-export const reservationsURL = 'http://localhost:3001/api/v1/reservations';
+export const reservationsURL = 'http://localhost:3001/api/v1/reservation';
 
 export default userURL;
