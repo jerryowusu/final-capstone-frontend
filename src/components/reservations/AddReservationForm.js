421f2e0709ff8dccd45f70a6d00412ec53d83ad1
Tobinchilongo <tobinchilongojnr@gmail.com>
2022-07-16 16:50:58 +0200

    Added Functionality to the `Reserve Car`

--- src/components/reservations/AddReservationForm.js
+++ src/components/reservations/AddReservationForm.js
@@ -1,13 +1,34 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+// import { useParams } from 'react-router-dom';
+import { reserveCar } from '../../redux/Reservations/reservation';
+import { getLocalStorage } from '../../logics/localStore';
 import './AddReservationForm.css';
 
 const AddReservationForm = () => {
   const [city, setCity] = useState('');
-  console.log(setCity);
+  const [date, setDate] = useState('');
+  const dispatch = useDispatch();
+  // const cars = useSelector((state) => state.allReservation.cars);
+  // const { id } = useParams();
+
+  // const myCar = cars.filter((car) => car.id === id);
+  // console.log(myCar);
+
+  const postData = (e) => {
+    e.preventDefault();
+    dispatch(reserveCar({
+      date,
+      city,
+      user_id: getLocalStorage().user_id,
+      car_id: 1,
+    }));
+  };
+
   return (
     <div className="form">
       <h2>Add Reservation</h2>
-      <form className="input">
+      <form onSubmit={postData} className="input">
         <div className="border-form">
           <select>
             <option>Car 1</option>
@@ -17,8 +38,14 @@ const AddReservationForm = () => {
             type="text"
             placeholder="Enter City"
             value={city}
+            onChange={(e) => setCity(e.target.value)}
           />
-          <input type="date" />
+          <input
+            type="text"
+            placeholder="Enter Date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
           <button type="submit">Reserve</button>
         </div>
       </form>
