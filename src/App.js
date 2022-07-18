58425490985dd025ce4a989184951ee96addbc62
cynthiainga <candyinga67@gmail.com>
2022-07-18 13:29:51 +0200

    Add the CarDetails and LoadingPage to the routes

--- src/App.js
+++ src/App.js
@@ -3,25 +3,34 @@ import './App.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Navbar from './components/Navbar';
 import Reservations from './redux/Reservations/reservation';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Homepage from './components/Homepage';
 import AddCar from './components/AddCar';
-import AddReservationForm from './components/reservations/AddReservationForm';
+import AddReservation from './components/Reservations/AddReservation';
 import DeleteCar from './components/DeleteCar';
+import CarDetails from './components/CarDetails';
+import LoadingPage from './components/LoadingPage';
 
 function App() {
+  const cars = useSelector((state) => state.allReservation.cars);
   return (
     <>
       <Navbar />
       <Routes>
-        <Route exact path="/" exactly element={<Homepage />} />
-        <Route path="/Reservations" component={Reservations} />
-        <Route path="/add_reservations" element={<AddReservationForm />} />
+        {!cars.length ? (
+          <Route path="/" element={<LoadingPage />} />
+        ) : (
+          <Route path="/" element={<Homepage />} />
+        )}
+        <Route exact path="/car/:id" exactly element={<CarDetails />} />
         <Route path="/add_car" element={<AddCar />} />
         <Route path="/delete_car" element={<DeleteCar />} />
+        <Route path="/add_reservations" element={<AddReservation />} />
+        <Route path="/Reservations" element={<Reservations />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registeration" element={<Signup />} />
       </Routes>
