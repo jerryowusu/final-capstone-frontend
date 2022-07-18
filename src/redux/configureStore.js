5bd92966b61e25794ec3edb68b713ab1a0b0e611
cynthiainga <candyinga67@gmail.com>
2022-07-18 13:26:49 +0200

    Import the uniqueCarReducer for carDetails

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,13 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import uniqueCarReducer from './Cars/carDetails';
 import carReducers from './Cars/cars';
 import reservationReducers from './Reservations/reservation';
 import userReducer from './user/user';
 
 const reducers = combineReducers({
-  carReducers, userReducer, allReservation: reservationReducers,
+  carReducers, userReducer, allReservation: reservationReducers, uniqueCarReducer,
 });
 
 const store = createStore(reducers, applyMiddleware(logger, thunk));
