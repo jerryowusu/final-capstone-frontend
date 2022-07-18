f60db4256190c42e0a9b332e66c3bb074179ef39
cynthiainga <candyinga67@gmail.com>
2022-07-18 13:28:08 +0200

    Add the CarDetails

---
+++ src/components/CarDetails.js
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+import { getUniqueCar } from '../redux/Cars/carDetails';
+import './card-details.scss';
+
+const CarDetails = () => {
+  const dispatch = useDispatch();
+  const id = useParams();
+  const idCAr = Number(id.id);
+  const cars = useSelector((state) => state.uniqueCarReducer);
+  useEffect(() => {
+    dispatch(getUniqueCar(idCAr));
+  }, []);
+
+  return (
+    <>
+      <div className="car_container" key={cars.id}>
+        <img src={cars.image_url} alt={cars.name} />
+        <div className="car_details">
+          <h2>{cars.name}</h2>
+          <ul>
+            <li>
+              Model:
+              {' '}
+              <span>{cars.model}</span>
+            </li>
+            <li className="price">
+              Price:
+              {' '}
+              <span>
+                {cars.price}
+                $
+              </span>
+            </li>
+            <li>
+              Availability:
+              {cars.reserved === false ? (
+                <span>No available</span>
+              ) : (
+                <span>available</span>
+              )}
+            </li>
+          </ul>
+          <span className="desc_short">Let us satisfy your desire!</span>
+          <Link to="/reservation">
+            <button
+              type="button"
+              className="reserve-btn"
+            >
+              Reserve Car
+            </button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CarDetails;
