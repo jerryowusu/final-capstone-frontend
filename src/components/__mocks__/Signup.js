2bf4a33dbebc0e69861f646a22287d035871afb8
cynthiainga <candyinga67@gmail.com>
2022-07-21 15:36:37 +0200

    Add mock for Signup page

---
+++ src/components/__mocks__/Signup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Signup = () => (
+  <section className="authentication-section">
+    <h1 className="register-page-heading">Sign Up</h1>
+    <form id="registeration-form" />
+  </section>
+);
+
+export default Signup;
